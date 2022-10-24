d29b7b11864432212a7a368788631cd44b83f5d6
Haram Park <harampark@kakao.com>
2022-10-24 17:49:36 +0900

    new post

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,6 +93,7 @@ export default {
       '/blog/seo-schema-jsonld/',
       '/blog/rdflib-tutorial-dcat-1',
       '/blog/rdflib-tutorial-dcat-2',
+      '/blog/python-sparql-endpoint',
     ]
   },
 
