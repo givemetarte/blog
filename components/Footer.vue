a8ac0f586edfb4b0dfdf8647220bec508d742eb1
Haram Park <harampark@kakao.com>
2023-01-13 21:12:54 +0900

    NEW POST

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
         <div class="max-w-5xl  mx-auto px-5 flex flex-col md:flex-row items-center justify-start md:justify-between">
             <ul class="flex flex-col md:flex-row text-sm space-y-2 md:space-y-0">
                 <li class="text-sm text-gray-700 mr-0 md:mr-3 flex justify-center"><nuxt-link to="#" class="hover:text-cherry">하람 블로그</nuxt-link></li>
-                <li class="hidden text-sm text-gray-700 mr-0 md:mr-3 flex justify-center md:block">|</li>
+                <li class="hidden text-sm text-gray-700 mr-0 md:mr-3 justify-center md:flex">|</li>
                 <li class="text-sm text-gray-700 mr-0 md:mr-3 flex justify-center"><a href="https://github.com/givemetarte" target="_blank"><GithubLogo /></a></li>
             </ul>
 
