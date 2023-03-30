13c267285dfc293271a9844336347087cacc3c3b
Haram Park <harampark@kakao.com>
2023-03-31 00:01:18 +0900

    add header

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
         <div class="max-w-5xl mx-auto flex items-center justify-between">
             <div class="max-w-fit ml-1.5 md:ml-0 hover:animate-wiggle hover:drop-shadow">
                 <nuxt-link to="/">
-                    <HeaderLogo />
+                    <HeaderLogo  />
                 </nuxt-link>
             </div>
             <div>
@@ -16,6 +16,16 @@
                     <li>
                         <div class="py-1 px-2 rounded-lg">
                             <nuxt-link :to="{path: '/about'}" replace class="text-gray-700 text-sm md:text-base hover:text-cherry poppins">About</nuxt-link>
+                        </div>
+                    </li>
+                    <li class="flex flex-row items-center justify-center group">
+                        <div class="pr-1 text-gray-700 text-sm md:text-base group-hover:text-cherry poppins">
+                            <a href="https://harampark.com" target="blank">Portfolio</a>
+                        </div>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 group-hover:text-cherry">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                            </svg>
                         </div>
                     </li>
                 </ul>
