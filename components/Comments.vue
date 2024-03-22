24fde31daef3284081080b14a2c1d052c0b698c9
Haram Park <52904037+givemetarte@users.noreply.github.com>
2024-03-22 16:10:13 +0900

    240322: minor edit

--- components/Comments.vue
+++ components/Comments.vue
@@ -5,6 +5,7 @@
 export default {
   mounted() {
     const scriptTag = document.createElement('SCRIPT')
+    // uterrances
     scriptTag.setAttribute('src', 'https://utteranc.es/client.js')
     scriptTag.setAttribute('repo', 'givemetarte/blog-comment')
     scriptTag.setAttribute('issue-term', 'pathname')
@@ -12,6 +13,22 @@ export default {
     scriptTag.setAttribute('theme', 'github-light')
     scriptTag.setAttribute('crossorigin', 'anonymous')
     scriptTag.setAttribute('async', 'async')
+    
+    // giscus
+    // scriptTag.setAttribute('src', 'https://giscus.app/client.js')
+    // scriptTag.setAttribute('repo', 'givemetarte/blog-comment')
+    // scriptTag.setAttribute('repo-id', 'R_kgDOGjgIew')
+    // scriptTag.setAttribute('data-category', 'General')
+    // scriptTag.setAttribute('data-category-id', 'DIC_kwDOGjgIe84CeJm1')
+    // scriptTag.setAttribute('data-mapping', 'pathname')
+    // scriptTag.setAttribute('data-strict', '0')
+    // scriptTag.setAttribute('data-reactions-enabled', '1')
+    // scriptTag.setAttribute('data-emit-metadata', '0')
+    // scriptTag.setAttribute('data-input-position', 'bottom')
+    // scriptTag.setAttribute('data-theme', 'preferred_color_scheme')
+    // scriptTag.setAttribute('data-lang', 'ko')
+    // scriptTag.setAttribute('crossorigin', 'anonymous')
+    // scriptTag.setAttribute('async', 'async')
     this.$el.append(scriptTag)
   },
 }
